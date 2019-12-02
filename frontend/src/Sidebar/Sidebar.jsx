d0678e4efa2105b3646a185a5da21f701745ce59
Radosław Rudziewicz <radek.rudziewicz4@wp.pl>
2019-12-02 21:04:28 +0100

    Sidebar

--- frontend/src/Sidebar/Sidebar.jsx
+++ frontend/src/Sidebar/Sidebar.jsx
@@ -1,268 +1,71 @@
 import React from 'react';
-
+import ItemSidebar from './ItemSidebar'
 class Sidebar extends React.Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-        return (
-            
+      
+    const diagramItems =[{
+            title:'Raport',
+            arrow:'',
+            subtitle:[{title:"Uwórz raport",link:"raport.html"},{title:"Test",link:"#"}],
+          }
+          ,{
+            title:'Test',
+            arrow:'113px',
+            subtitle:[{title:'Test', link:"#"},{title:'Test',link:"#"}]
+          }]
 
+    const statystykaItems=[{
+          title:'Test',
+          arrow:'',
+          subtitle:[{title:"Test",link:"#"},{title:"Test",link:"#"}],
+          }
+        ,{
+          title:'Test',
+          arrow:'114px',
+          subtitle:[{title:"Test",link:"#"},{title:"Test",link:"#"}],
+          }
+        ,{
+          title:'Test',
+          arrow:'166px',
+          subtitle:[{title:"Test",link:"#"},{title:"Test",link:"#"}],
+          }
+        ,{
+          title:'Test',
+          arrow:'220px',
+          subtitle:[{title:"Test",link:"#"},{title:"Test",link:"#"}],
+        }
+        ,{
+          title:'Test',
+          arrow:'272px',
+          subtitle:[{title:"Test",link:"#"},{title:"Test",link:"#"}],
+        }
+        ,{
+          title:'Test',
+          arrow:'323px',
+          subtitle:[{title:"Test",link:"#"},{title:"Test",link:"#"}],
+        }
+        ,{
+          title:'Test',
+          arrow:'377px',
+          subtitle:[{title:"Test",link:"#"},{title:"Test",link:"#"}],
+        }
+      ]  
+      
+        return (
+       
       <div data-component="sidebar">
         <div className="sidebar">
           <ul className="list-group flex-column d-inline-block first-menu">
-            <li className="list-group-item pl-3 py-2">
-              <a href="#"><i className="fas fa-project-diagram" aria-hidden="true"><span className="ml-2 align-middle">Projekty</span></i></a>
-              <ul className="list-group flex-column d-inline-block submenu">
-                <li className="list-group-item pl-4">
-                  <a href="#" >Raporty</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" />
-                    <li className="list-group-item pl-4">
-                      <a href="raport.html">Utwórz raport</a>
-                    </li>
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" style={{top: '113px'}} />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li className="list-group-item pl-3 py-2">
-              <a href="#"><i className="fas fa-chart-pie" aria-hidden="true"><span className="ml-2 align-middle">Statystyka</span></i></a>
-              <ul className="list-group flex-column d-inline-block submenu">
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" style={{top: '114px'}} />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" style={{top: '166px'}} />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" style={{top: '220px'}} />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" style={{top: '272px'}} />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" style={{top: '323px'}} />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" style={{top: '377px'}} />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li className="list-group-item pl-3 py-2">
-              <a href="#">
-                <i className="fas fa-address-book" aria-hidden="true"><span className="ml-2 align-middle">Użytkownicy</span></i>
-              </a>
-              <ul className="list-group flex-column d-inline-block submenu">
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" style={{top: '114px'}} />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" style={{top: '166px'}} />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li className="list-group-item pl-3 py-2">
-              <a href="#"><i className="fas fa-terminal" aria-hidden="true"><span className="ml-2 align-middle">Terminal</span></i></a>
-              <ul className="list-group flex-column d-inline-block submenu">
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" style={{top: '114px'}} />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li className="list-group-item pl-3 py-2">
-              <a href="#"><i className="fas fa-microchip" aria-hidden="true"><span className="ml-2 align-middle">Elektronika</span></i></a>
-              <ul className="list-group flex-column d-inline-block submenu">
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" style={{top: '114px'}} />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" style={{top: '166px'}} />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" style={{top: '220px'}} />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" style={{top: '272px'}} />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" style={{top: '323px'}} />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" style={{top: '377px'}} />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li className="list-group-item pl-3 py-2">
-              <a href="#"><i className="fas fa-bath" aria-hidden="true"><span className="ml-2 align-middle">Prysznic</span></i></a>
-              <ul className="list-group flex-column d-inline-block submenu">
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" style={{top: '114px'}} />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="list-group-item pl-4">
-                  <a href="#">Test</a>
-                  <ul className="list-group flex-column d-inline-block sub-submenu">
-                    <span className="arrow" style={{top: '166px'}} />
-                    <li className="list-group-item pl-4">
-                      <a href="#">Test</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
+           <ItemSidebar style="fas fa-project-diagram" items={diagramItems} header='Projekty'/>
+           <ItemSidebar style="fas fa-chart-pie" items={statystykaItems} header='Statystyka'/>
+           <ItemSidebar style="fas fa-address-book" items={statystykaItems} header='Użytkownicy'/>
+           <ItemSidebar style="fas fa-terminal" items={statystykaItems} header='Terminal'/>
+           <ItemSidebar style="fas fa-microchip" items={statystykaItems} header='Elektronika'/>
+           <ItemSidebar style="fas fa-bath"  items={statystykaItems} header='Prysznic'/>
           </ul>
         </div>
       </div>
