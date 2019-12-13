f46bc4265ca6d99697c1f19ee09a693c59a6ff4b
Patryk-S-W <10290008+Patryk-S-W@users.noreply.github.com>
2019-12-13 01:18:14 +0100

    fix database

--- backend/database/queries.js
+++ backend/database/queries.js
@@ -5,6 +5,8 @@ const pool = new Pool({
 	database: 'api',
 	password: 'admin',
 	port: 5432,
+	max: 10,
+	idleTimeoutMillis: 30000,
 })
 
 
@@ -17,13 +19,53 @@ const getUsers = (request, response) => {
 	})
 }
 
-const createUser = (request, response) => {
-	pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+const getUserById = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
 		if (error) {
 			throw error
 		}
 		response.status(200).json(results.rows)
 	})
+}
+
+const createUser = (request, response) => {
+	const { name, email } = request.body
+
+	pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+		if (error) {
+			throw error
+		}
+		response.status(201).send(`User added with ID: ${result.insertId}`)
+	})
+}
+
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { name, email } = request.body
+
+  pool.query(
+    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
+
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
 }
 
 const createProject = (request, response) => {
@@ -82,7 +124,10 @@ const getProjects = (request, response) => {
 
 module.exports = {
 	getUsers,
+	getUserById,
 	createUser,
+	updateUser,
+	deleteUser,
 	createProject,
 	createRaport,
 	createSchedule,
--- backend/server.js
+++ backend/server.js
@@ -6,18 +6,38 @@ const bodyParser = require('body-parser');
 const errorHandler = require('_helpers/error-handler');
 const db = require('./database/queries');
 const multer = require('multer')
+const helmet = require('helmet')
+const compression = require('compression')
+const rateLimit = require('express-rate-limit')
+const { body, check } = require('express-validator')
 
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(compression())
+app.use(helmet())
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 10,
+})
+
+app.use(limiter)
 
 app.get('/test', db.getUsers)
 
 // api routes
-app.use('/users', require('./users/users.controller'));
-
+//app.use('/users', require('./users/users.controller'));
+app.get('/', (request, response) => {
+  response.json({ info: 'API' })
+})
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
 // file upload
 const storage = multer.diskStorage({
@@ -49,7 +69,13 @@ app.post('/uploadRaports', function(req, res) {
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+
+const isProduction = process.env.NODE_ENV === 'production'
+const origin = {
+  origin: isProduction ? 'https://projektygrupowe.ftims.pg.edu.pl' : '*',
+}
+app.use(cors(origin))
+const port = isProduction ? 80 : 4000;
 const server = app.listen(port, function() {
 	console.log('Server listening on port ' + port);
 });
